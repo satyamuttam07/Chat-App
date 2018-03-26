67f8de7239bd360079ccc632d06398d312041888
Satyam <satyamuttam07@gmail.com>
2018-03-27 00:02:01 +0530

    front end and

--- public/js/index.js
+++ public/js/index.js
@@ -13,6 +13,15 @@
 		jQuery('#messages').append(li);
 	});
 
+	socket.on('newLocationMessage', function (message) {
+		var li = jQuery('<li></li>');
+		var a = jQuery('<a target="_blank">My location</a>');
+
+		li.text(`${message.from}: `);
+		a.attr('href',message.url);
+		li.append(a);
+		jQuery('#messages').append(li);
+	});
 	jQuery('#message-form').on('submit',function (e) {
 		e.preventDefault();
 
@@ -20,6 +29,24 @@
 			from : 'user',
 			text : jQuery('[name=message]').val()
 		}, function () {
+			jQuery('[name=message]').val('')
+		});
+	});
 
+	var locationButton = jQuery('#send-location');
+	locationButton.on('click', function () {
+		if(!navigator.geolocation) {
+			return alert('geolocation not supported by your browser');
+		}
+		locationButton.attr('disabled', 'disabled').text('Sending...');
+		navigator.geolocation.getCurrentPosition(function (position) {
+			locationButton.removeAttr('disabled').text('Send Location');
+			socket.emit('createLocationMessage', {
+				latitude: position.coords.latitude,
+				longitude: position.coords.longitude 
+			});
+		}, function() {
+			locationButton.removeAttr('disabled').text('Send Location');
+			alert('Unable to fetch the loaction');
 		});
 	});
