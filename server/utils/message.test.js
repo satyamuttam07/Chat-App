045c395420212d2f463ba1a862da00743630f68d
Satyam <satyamuttam07@gmail.com>
2018-04-10 18:17:06 +0530

    test feild updated

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -9,6 +9,6 @@ describe('generateMessage', () => {
 		var message = generateMessage(from,text);
 
 		expect(message.createdAt).toBeA('number');
-		//expect(message).toInclude({from, text});
+		expect(message).toInclude({from, text});
 	});
 });
